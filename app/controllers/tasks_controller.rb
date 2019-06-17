9784f2ca1d0526eebb1b84ed763bfc0e63941042
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-06-17 12:17:00 +0300

    Beginning of CRUD Actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+end
